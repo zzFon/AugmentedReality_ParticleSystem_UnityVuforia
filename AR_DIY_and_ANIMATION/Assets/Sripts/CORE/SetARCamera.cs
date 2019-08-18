148c3110f4e809675622455f789c192a6de1dcf6
zzFon <735239697@qq.com>
2019-08-19 00:01:49 +0800

    2019-8-19 00:01 edition1 /Camera Auto Focusing

--- AR_DIY_and_ANIMATION/Assets/Sripts/CORE/SetARCamera.cs
+++ AR_DIY_and_ANIMATION/Assets/Sripts/CORE/SetARCamera.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
 
 public class SetARCamera : MonoBehaviour 
 {
@@ -11,5 +12,27 @@ public class SetARCamera : MonoBehaviour
 		arcam.transform.position = new Vector3 (0.0f, 0.5f, 0.0f);
 		arcam.transform.rotation = Quaternion.identity;
 		arcam.transform.Rotate (new Vector3 (90, 0, 0));
+	}
+
+	void Start () 
+	{
+		VuforiaARController.Instance.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+		VuforiaARController.Instance.RegisterOnPauseCallback(OnPaused);
+	}
+
+	private void OnVuforiaStarted()
+	{
+		CameraDevice.Instance.SetFocusMode(
+			CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+	}
+
+	private void OnPaused(bool paused)
+	{
+		if (!paused) // resumed
+		{
+			// Set again autofocus mode when app is resumed
+			CameraDevice.Instance.SetFocusMode(
+				CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+		}
 	}
 }
